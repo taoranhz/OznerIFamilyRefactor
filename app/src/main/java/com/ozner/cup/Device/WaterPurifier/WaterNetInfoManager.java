85b14940dfa06f478cbbfa6bdf492cb65c158332
QX-Zach <767231470@qq.com>
2016-11-16 14:23:13 +0800

    测试提交

---
+++ app/src/main/java/com/ozner/cup/Device/WaterPurifier/WaterNetInfoManager.java
@@ -0,0 +1,11 @@
+package com.ozner.cup.Device.WaterPurifier;
+
+/**
+ * Created by ozner_67 on 2016/11/16.
+ * 邮箱：xinde.zhang@cftcn.com
+ * <p>
+ * 净水器网络信息管理
+ */
+
+public class WaterNetInfoManager {
+}
