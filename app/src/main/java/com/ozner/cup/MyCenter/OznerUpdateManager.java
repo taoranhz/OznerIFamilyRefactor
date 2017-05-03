1d65a7d3be8a5fba9259e19dbf7174099be8fa44
QX-Zach <767231470@qq.com>
2017-05-03 11:32:35 +0800

    优化应用更新

--- app/src/main/java/com/ozner/cup/MyCenter/OznerUpdateManager.java
+++ app/src/main/java/com/ozner/cup/MyCenter/OznerUpdateManager.java
@@ -6,9 +6,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -18,6 +20,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
+import com.ozner.cup.BuildConfig;
 import com.ozner.cup.HttpHelper.ApiException;
 import com.ozner.cup.HttpHelper.HttpMethods;
 import com.ozner.cup.HttpHelper.OznerHttpResult;
@@ -109,6 +112,7 @@ public class OznerUpdateManager {
                                     String updateContent = data.get("updatecon").getAsString();
                                     String netVersionName = data.get("versionname").getAsString();
                                     installPackagename = downLoadUrl.substring(downLoadUrl.lastIndexOf("/") + 1);
+                                    installPackagename = installPackagename.replace(" ","");
                                     if (netVersionCode > getVersionCode(mContext.get())) {
                                         showNoticeDialog(updateContent);
                                     } else {
@@ -286,21 +290,48 @@ public class OznerUpdateManager {
         }
     };
 
+
+
     /**
      * 安装apk
      *
      * @param
      */
     private void installApk() {
-        File apkfile = new File(folderPath + "/" + installPackagename);
+        File apkfile = new File(folderPath, installPackagename);
         if (!apkfile.exists()) {
             return;
         }
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        Log.e("tag", "installApk: Slience:" + Uri.parse("file://" + apkfile.toString()));
-        i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
-        mContext.get().startActivity(i);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(mContext.get(), BuildConfig.APPLICATION_ID + ".fileprovider", apkfile);
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+            LCLogUtils.E(TAG, "contentUri:" + contentUri);
+        } else {
+            intent.setDataAndType(Uri.parse("file://" + apkfile.getAbsolutePath()), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            LCLogUtils.E(TAG, "installApk: Slience:" + Uri.parse("file://" + apkfile.toString()));
+        }
+
+        mContext.get().startActivity(intent);
 
     }
+//    /**
+//     * 安装apk
+//     *
+//     * @param
+//     */
+//    private void installApk() {
+//        File apkfile = new File(folderPath + "/" + installPackagename);
+//        if (!apkfile.exists()) {
+//            return;
+//        }
+//        Intent i = new Intent(Intent.ACTION_VIEW);
+//        Log.e("tag", "installApk: Slience:" + Uri.parse("file://" + apkfile.toString()));
+//        i.setDataAndType(Uri.parse("file://" + apkfile.toString()), "application/vnd.android.package-archive");
+//        mContext.get().startActivity(i);
+//
+//    }
 
 }
